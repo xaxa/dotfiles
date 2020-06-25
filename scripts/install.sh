695e36123c665f36025f9b0f6f28a24bfb4ee742
Vinicius XaXa <vinicius.xaxa@gmail.com>
2020-06-25 16:22:37 -0300

    adicionando docker no script

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,6 +26,18 @@ if [ -d "$DOTFILES_DIRECTORY" ]; then
 	ln -s $DOTFILES_DIRECTORY/.zshrc ~/.zshrc
 	touch ~/.zshrc.local
 	source ~/.zshrc
+
+
+	#docker
+	sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	#docker engine
+	sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+
+
 else
 	echo "$DOTFILES_DIRECTORY nao existe. Vc ainda nao baixou seu repositorio dotfile!" 
 	chsh -s $(which zsh)
