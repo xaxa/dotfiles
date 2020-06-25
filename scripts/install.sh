ecef543a64823499c8bf269022b008653d2213e9
Vinicius XaXa <vinicius.xaxa@gmail.com>
2020-06-25 16:36:24 -0300

    adicionando docker compose no script de instalacao

--- scripts/install.sh
+++ scripts/install.sh
@@ -35,6 +35,9 @@ if [ -d "$DOTFILES_DIRECTORY" ]; then
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	#docker engine
 	sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+	#docker compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
 
 
 
