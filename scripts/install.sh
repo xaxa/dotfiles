9ec24642e33ec7c2270063f8695765a46f39c1ec
Vinicius XaXa <vinicius.xaxa@gmail.com>
2020-06-25 21:05:26 -0300

    permissao docker

--- scripts/install.sh
+++ scripts/install.sh
@@ -38,6 +38,7 @@ if [ -d "$DOTFILES_DIRECTORY" ]; then
 	#docker compose
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
+	sudo usermod -aG docker ${USER}
 
 
 
